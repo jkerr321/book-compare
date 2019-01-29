c3ca99656226ef3445b77a13dd456bfb39f5e14f
Jennifer Kerr <jkerr321@gmail.com>
2019-01-29 12:02:16 +0000

    add dynamic isbn into pageScraper and controller

--- server/controllers/price-compare-controller.js
+++ server/controllers/price-compare-controller.js
@@ -6,13 +6,14 @@ const renderPriceCompareTable = async (req, res) => {
     try {
         const testMode = true //TODO, make this conditional?
         const toReadList = await getGoodReadBooks();
-        
+
         // for each toRead get price info and add to book object
         const booksWithPrices = toReadList.map(async bookInfo => {
             try {
                 const bookModel = new BookModel;
                 bookModel.addBookInfo(bookInfo);
-                const prices = await scrapePrices({isbn: bookInfo.isbn, bookModel, testMode:testMode});
+                const isbn = bookModel.model.isbn ? bookModel.model.isbn : bookModel.model.isbn13;
+                const prices = await scrapePrices({isbn, bookModel, testMode: testMode});
                 // bookModel.addPriceInfo(prices); //TODO instead of inside lib?
                 return bookModel.model;
             } catch(err) {
--- server/lib/page-scraper.js
+++ server/lib/page-scraper.js
@@ -4,46 +4,52 @@ const testData = require('../test/fixtures/testData');
 
 const pageScraper = async ({isbn, bookModel, testMode}) => {
     try {
-        //scrape prices from amazon (or testData if in test mode)
-        const options = {
-            //TODO use isbn here
-            uri: 'https://www.amazon.co.uk/gp/product/1781258635/ref=x_gr_w_bb?ie=UTF8&tag=x_gr_w_bb_uk-21&linkCode=as2&camp=1634&creative=6738',
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-            }
-        }
-        const html = testMode ? testData : await rp(options);
-        const $ = cheerio.load(html);
+        if (isbn) {
+            // get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
+            const aisn = isbn.substring(isbn.length - 10);
 
-        // isolate section of html containing prices and use regex to extract them
-        $('#twister > .top-level').map((i, el)=>{
-            const strWithWhitespace = $(el).text(); //?? why console.log($(this).text()) not work? nothing is logged
-            const cleanString = strWithWhitespace.replace(/\s+/g, ' ').trim()
-
-            // extract book format (kindle, hardcover etc)
-            const formatRegex = /([a-z]+)/gmi;
-            const amazonFormat = cleanString.match(formatRegex)[0];
-
-            // extract prices
-            const priceRegex = /(—|[0-9.]+)/gmi;
-            prices = cleanString.match(priceRegex);
-      
-            const amazonPriceObject = {
-                [amazonFormat]: {
-                    amazon: prices[0],
-                    new_from: prices[1],
-                    used_from: prices[2]
+            //scrape prices from amazon (or testData if in test mode)
+            const options = {
+                //TODO use isbn here
+                uri: `https://www.amazon.co.uk/gp/product/${aisn}`,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
                 }
             }
+            const html = testMode ? testData : await rp(options);
+            const $ = cheerio.load(html);
 
-            Object.keys(bookModel.model.prices).forEach(bookModelFormat => {
-                if (amazonFormat.toLowerCase() === bookModelFormat) {
-                    bookModel.model.prices[bookModelFormat] = amazonPriceObject;
+            // isolate section of html containing prices and use regex to extract them
+            $('#twister > .top-level').map((i, el)=>{
+                const strWithWhitespace = $(el).text(); //?? why console.log($(this).text()) not work? nothing is logged
+                const cleanString = strWithWhitespace.replace(/\s+/g, ' ').trim()
+
+                // extract book format (kindle, hardcover etc)
+                const formatRegex = /([a-z]+)/gmi;
+                const amazonFormat = cleanString.match(formatRegex)[0];
+
+                // extract prices
+                const priceRegex = /(—|[0-9.]+)/gmi;
+                prices = cleanString.match(priceRegex);
+        
+                const amazonPriceObject = {
+                    [amazonFormat]: {
+                        amazon: prices[0],
+                        new_from: prices[1],
+                        used_from: prices[2]
+                    }
                 }
+
+                Object.keys(bookModel.model.prices).forEach(bookModelFormat => {
+                    if (amazonFormat.toLowerCase() === bookModelFormat) {
+                        bookModel.model.prices[bookModelFormat] = amazonPriceObject;
+                    }
+                });
             });
-        });
 
-        return bookModel;
+            return bookModel;
+        } 
+        //TODO handle else situation
 
     } catch (err) {
         console.log(err);
