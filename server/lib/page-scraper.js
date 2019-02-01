c2310c659b6b9af8bbe3af9eea9f563d992721dc
Jennifer Kerr <jkerr321@gmail.com>
2019-02-01 18:41:33 +0000

    basic pageScraper works with puppeteer, need to add back  in bookModel mapping, test mode, and dynamic isbn

--- server/lib/page-scraper.js
+++ server/lib/page-scraper.js
@@ -1,59 +1,92 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const testData = require('../test/fixtures/testData');
 
-const pageScraper = async ({isbn, bookModel, testMode}) => {
-    try {
-        if (isbn) {
-            // get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
-            const aisn = isbn.substring(isbn.length - 10);
+//THIS WORKS!
+const pageScraper = async () => {
+    const url = `https://www.amazon.co.uk/gp/product/1781258635`;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 926 });
+    await page.goto(url);
 
-            //scrape prices from amazon (or testData if in test mode)
-            const options = {
-                //TODO use isbn here
-                uri: `https://www.amazon.co.uk/gp/product/${aisn}`,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+   const result  = await page.$$eval('#twister > .top-level', element => {
+        const htmlArray = element.map(el => el.innerText);
+        return htmlArray.map(el => {
+            const cleanString = el.replace(/\s+/g, ' ').trim()
+
+            // extract book format (kindle, hardcover etc)
+            const formatRegex = /([a-z]+)/gmi;
+            const amazonFormat = cleanString.match(formatRegex)[0];
+
+            // extract prices
+            const priceRegex = /(—|[0-9.]+)/gmi;
+            prices = cleanString.match(priceRegex);
+
+            const amazonPriceObject = {
+                [amazonFormat]: {
+                    amazon: prices[0],
+                    new_from: prices[1],
+                    used_from: prices[2]
                 }
             }
-            const html = testMode ? testData : await rp(options);
-            const $ = cheerio.load(html);
+            return amazonPriceObject;
+        })
+    });
 
-            // isolate section of html containing prices and use regex to extract them
-            $('#twister > .top-level').map((i, el)=>{
-                const strWithWhitespace = $(el).text(); //?? why console.log($(this).text()) not work? nothing is logged
-                const cleanString = strWithWhitespace.replace(/\s+/g, ' ').trim()
+    console.log('result', result);
 
-                // extract book format (kindle, hardcover etc)
-                const formatRegex = /([a-z]+)/gmi;
-                const amazonFormat = cleanString.match(formatRegex)[0];
+};
 
-                // extract prices
-                const priceRegex = /(—|[0-9.]+)/gmi;
-                prices = cleanString.match(priceRegex);
-        
-                const amazonPriceObject = {
-                    [amazonFormat]: {
-                        amazon: prices[0],
-                        new_from: prices[1],
-                        used_from: prices[2]
-                    }
-                }
+pageScraper();
 
-                Object.keys(bookModel.model.prices).forEach(bookModelFormat => {
-                    if (amazonFormat.toLowerCase() === bookModelFormat) {
-                        bookModel.model.prices[bookModelFormat] = amazonPriceObject;
-                    }
-                });
-            });
+// const pageScraper = async ({isbn, bookModel, testMode}) => {
+//     try {
+//         if (isbn) {
+//             // get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
+//             const aisn = isbn.substring(isbn.length - 10);
 
-            return bookModel;
-        } 
-        //TODO handle else situation
+//             //scrape prices from amazon (or testData if in test mode)
+//             const url = `https://www.amazon.co.uk/gp/product/${aisn}`;
+//             const browser = await puppeteer.launch({ headless: true });
+//             const page = await browser.newPage();
+//             await page.setViewport({ width: 1920, height: 926 });
+//             await page.goto(url);
 
-    } catch (err) {
-        console.log(err);
-    }
-}
+//             //TODO put test mode back in for html
+//             await page.$$eval('#twister > .top-level', element => {
+//                 const each = element.map(el => {
+//                     const strWithWhitespace = el.outerHTML;
+//                     const cleanString = strWithWhitespace.replace(/\s+/g, ' ').trim()
 
-module.exports = pageScraper;
+//                     // extract book format (kindle, hardcover etc)
+//                     const formatRegex = /([a-z]+)/gmi;
+//                     const amazonFormat = cleanString.match(formatRegex)[0];
+
+//                     // extract prices
+//                     const priceRegex = /(—|[0-9.]+)/gmi;
+//                     prices = cleanString.match(priceRegex);
+
+//                     const amazonPriceObject = {
+//                         [amazonFormat]: {
+//                             amazon: prices[0],
+//                             new_from: prices[1],
+//                             used_from: prices[2]
+//                         }
+//                     }
+
+//                     Object.keys(bookModel.model.prices).forEach(bookModelFormat => {
+//                         if (amazonFormat.toLowerCase() === bookModelFormat) {
+//                             bookModel.model.prices[bookModelFormat] = amazonPriceObject;
+//                         }
+//                     });
+//                 });
+//             });
+
+//             return bookModel;
+//         } //TODO handle else situation
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// module.exports = pageScraper;
