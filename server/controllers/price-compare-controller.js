dce93f8bf6b9a5c40aabb2d013d49437a72f2391
Jennifer Kerr <jkerr321@gmail.com>
2019-02-15 16:40:00 +0000

    playing around

--- server/controllers/price-compare-controller.js
+++ server/controllers/price-compare-controller.js
@@ -1,26 +1,23 @@
 const scrapePrices = require('../lib/page-scraper');
 const getGoodReadBooks = require('../lib/good-reads-api');
 const BookModel = require('../models/book-model');
+const goodReadsTestResponse = require('../test/fixtures/good-reads-api-output');
 const testModel = require('../test/fixtures/complete-book-model-view');
 
 const renderPriceCompareTable = async (req, res) => {
     try {
         // const toReadList = await getGoodReadBooks();
+        const toReadList = await goodReadsTestResponse;
 
-        // // for each toRead get price info and add to book object
-        // const booksWithPrices = toReadList2.map(async bookInfo => {
-        //     try {
-        //         const bookModel = new BookModel;
-        //         bookModel.addBookInfo(bookInfo);
-        //         const isbn = bookModel.model.isbn ? bookModel.model.isbn : bookModel.model.isbn13;
-        //         const prices = await scrapePrices({isbn, bookModel, testMode: testMode});
-        //         // bookModel.addPriceInfo(prices); //TODO instead of inside lib?
-        //         return bookModel.model;
-        //     } catch(err) {
-        //         console.log(err);
-        //     }
-        // })
+        // for each toRead get price info and add to book object
+        const bookModelArray = toReadList.map(bookInfo => {
+            const bookModel = new BookModel;
+            bookModel.addBookInfo(bookInfo);
+            return bookModel;
+        });
 
+        await scrapePrices(bookModelArray);
+        
         // const books = await Promise.all(booksWithPrices);
         const books = testModel;
         res.render('page', { books });
--- server/lib/page-scraper.js
+++ server/lib/page-scraper.js
@@ -1,53 +1,66 @@
 const puppeteer = require('puppeteer');
 
-const pageScraper = async ({ isbn, bookModel, testMode }) => {
-    try {
-        if (isbn) {
-            // get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
-            const aisn = isbn.substring(isbn.length - 10);
+const pageScraper = async ( bookModelArray ) => {
+    const browser = await puppeteer.launch();
 
-            //scrape prices from amazon using puppeteer (or testData if in test mode)
-            const url = `https://www.amazon.co.uk/gp/product/${aisn}`;
-            const browser = await puppeteer.launch({ headless: true });
-            const page = await browser.newPage();
-            await page.setViewport({ width: 1920, height: 926 });
-            await page.goto(url);
-            // array of each row of amazon book format price table
-            const htmlArray = await page.$$eval('#twister > .top-level', element => element.map(el => el.innerText));            
-            
-            htmlArray.map(el => {
-                //remove whitespace
-                const cleanString = el.replace(/\s+/g, ' ').trim()
+    bookModelArray.forEach(async bookModel => {
+        const isbn = bookModel.model.isbn ? bookModel.model.isbn : bookModel.model.isbn13;
 
-                // extract book format (kindle, hardcover etc)
-                const formatRegex = /([a-z]+)/gmi;
-                const amazonFormat = cleanString.match(formatRegex)[0];
+        try {
+            if (isbn) {
+                // get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
+                const aisn = isbn.substring(isbn.length - 10);
 
-                // extract prices
-                const priceRegex = /(—|[0-9.]+)/gmi;
-                prices = cleanString.match(priceRegex);
+                //scrape prices from amazon using puppeteer (or testData if in test mode)
+                const url = `https://www.amazon.co.uk/gp/product/${aisn}`;
+                const page = await browser.newPage();
+                await page.setViewport({ width: 1920, height: 926 });
+                await page.goto(url);
+                // array of each row of amazon book format price table
+                const htmlArray = await page.$$eval('#twister > .top-level', element => element.map(el => el.innerText));            
+                await page.close();
+                
+                htmlArray.map(el => {
+                    //remove whitespace
+                    const cleanString = el.replace(/\s+/g, ' ').trim()
 
-                const amazonPrices = {
-                    amazon: prices[0],
-                    new_from: prices[1],
-                    used_from: prices[2]
-                }
+                    // extract book format (kindle, hardcover etc)
+                    const formatRegex = /([a-z]+)/gmi;
+                    const amazonFormat = cleanString.match(formatRegex)[0];
 
-                // add prices to book model
-                Object.keys(bookModel.model.prices).forEach(bookModelFormat => {
-                    if (amazonFormat.toLowerCase() === bookModelFormat) {
-                        bookModel.model.prices[bookModelFormat] = amazonPrices;
+                    // extract prices
+                    const priceRegex = /(—|[0-9.]+)/gmi;
+                    prices = cleanString.match(priceRegex);
+
+                    const amazonPrices = {
+                        amazon: prices[0],
+                        new_from: prices[1],
+                        used_from: prices[2]
                     }
-                });
-            })
-            
-            return bookModel;
-        } else {
-            console.log(`no isbn for ${bookModel.title}`) //TODO handle this better
+
+                    console.log('==================');
+                    console.log('amazonPrices', amazonPrices);
+                    console.log('==================');
+                    
+
+                    // add prices to book model
+                    Object.keys(bookModel.model.prices).forEach(bookModelFormat => {
+                        if (amazonFormat.toLowerCase() === bookModelFormat) {
+                            bookModel.model.prices[bookModelFormat] = amazonPrices;
+                        }
+                    });
+                })
+                
+                return bookModel;
+            } else {
+                console.log(`no isbn for ${bookModel.model.title}`) //TODO handle this better
+            }
+        } catch (error) {
+            console.log('error', error);
         }
-    } catch (error) {
-        console.log('error', error);
-    }
+    });
+
+    // await browser.close();
 };
 
 module.exports = pageScraper;
