73a256ab87335693582ada2b3a17fbe9a36abb23
Jennifer Kerr <jkerr321@gmail.com>
2019-03-01 09:12:56 +0000

    Refactor so bookModel is no longer required
    Reduce side effects

--- server/test/fixtures/good-reads-api-output.js
+++ server/test/fixtures/good-reads-api-output.js
@@ -1,57 +1,58 @@
-module.exports = [{
-        title: 'Natives: Race and Class in the Ruins of Empire',
-        author: 'Akala',
-        average_rating: '4.61',
-        isbn: null,
-        isbn13: '9781473661219'
-    },
-    {
-        title: 'The Color Purple',
-        author: 'Alice Walker',
-        average_rating: '4.19',
-        isbn: '0671727796',
-        isbn13: '9780671727796'
-    },
-    {
-        title: 'How Do You Like Me Now?',
-        author: 'Holly Bourne',
-        average_rating: '3.91',
-        isbn: '1473667720',
-        isbn13: '9781473667723'
-    },
-    {
-        title: 'Frenchman\'s Creek',
-        author: 'Daphne du Maurier',
-        average_rating: '3.93',
-        isbn: '1844080412',
-        isbn13: '9781844080410'
-    },
-    {
-        title: 'Natives: Race and Class in the Ruins of Empire2',
-        author: 'Akala',
-        average_rating: '4.61',
-        isbn: null,
-        isbn13: '9781473661219'
-    },
-    {
-        title: 'The Color Purple2',
-        author: 'Alice Walker',
-        average_rating: '4.19',
-        isbn: '0671727796',
-        isbn13: '9780671727796'
-    },
-    {
-        title: 'How Do You Like Me Now?2',
-        author: 'Holly Bourne',
-        average_rating: '3.91',
-        isbn: '1473667720',
-        isbn13: '9781473667723'
-    },
-    {
-        title: 'Frenchman\'s Creek2',
-        author: 'Daphne du Maurier',
-        average_rating: '3.93',
-        isbn: '1844080412',
-        isbn13: '9781844080410'
-    }
+module.exports = [
+	{
+		title: 'Natives: Race and Class in the Ruins of Empire',
+		author: 'Akala',
+		average_rating: '4.61',
+		isbn: null,
+		isbn13: null
+	},
+	{
+		title: 'The Color Purple',
+		author: 'Alice Walker',
+		average_rating: '4.19',
+		isbn: '0671727796',
+		isbn13: '9780671727796'
+	},
+	{
+		title: 'How Do You Like Me Now?',
+		author: 'Holly Bourne',
+		average_rating: '3.91',
+		isbn: '1473667720',
+		isbn13: '9781473667723'
+	},
+	{
+		title: "Frenchman's Creek",
+		author: 'Daphne du Maurier',
+		average_rating: '3.93',
+		isbn: '1844080412',
+		isbn13: '9781844080410'
+	},
+	{
+		title: 'Natives: Race and Class in the Ruins of Empire2',
+		author: 'Akala',
+		average_rating: '4.61',
+		isbn: null,
+		isbn13: null
+	},
+	{
+		title: 'The Color Purple2',
+		author: 'Alice Walker',
+		average_rating: '4.19',
+		isbn: '0671727796',
+		isbn13: '9780671727796'
+	},
+	{
+		title: 'How Do You Like Me Now?2',
+		author: 'Holly Bourne',
+		average_rating: '3.91',
+		isbn: '1473667720',
+		isbn13: '9781473667723'
+	},
+	{
+		title: "Frenchman's Creek2",
+		author: 'Daphne du Maurier',
+		average_rating: '3.93',
+		isbn: '1844080412',
+		isbn13: '9781844080410'
+	}
 ];
