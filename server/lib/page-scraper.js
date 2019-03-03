a9ab71ef0d0f7857529875394138b8bf0dac651a
Jennifer Kerr <jkerr321@gmail.com>
2019-03-03 11:28:07 +0000

    Use node-fetch to retrieve html In order to avoid timeout errors experienced with puppeteer

--- server/lib/page-scraper.js
+++ server/lib/page-scraper.js
@@ -1,27 +1,37 @@
-const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+const { COOKIE } = require('../../config');
 
 const getData = async (aisn, browser) => {
 	try {
-		//scrape prices from amazon using puppeteer
 		const url = `https://www.amazon.co.uk/gp/product/${aisn}`;
-		const page = await browser.newPage();
-		await page.setViewport({
-			width: 1920,
-			height: 926
-		});
-		// await page.goto(url);
-		await page.goto(url, {
-			timeout: 0,
-			waitUntil: 'networkidle2'
+		const headers = {
+			authority: 'www.amazon.co.uk',
+			method: 'GET',
+			path: `/gp/product/${aisn}`,
+			scheme: 'https',
+			accept:
+				'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+			'accept-encoding': 'gzip, deflate, br',
+			'accept-language': 'en-US,en;q=0.9',
+			'cache-control': 'max-age=0',
+			cookie: COOKIE,
+			'upgrade-insecure-requests': 1,
+			'user-agent':
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'
+		};
+
+		const res = await fetch(url, { headers });
+		const text = await res.text();
+		const $ = cheerio.load(text);
+
+		// TODO put this in a function to be more functional
+		let arrayOfHtmlPriceTableRows = [];
+		$('#twister > .top-level').map((i, el) => {
+			arrayOfHtmlPriceTableRows.push($(el).text());
 		});
 
-		// array of each row of amazon price table
-		const htmlArray = await page.$$eval('#twister > .top-level', element =>
-			element.map(el => el.innerText)
-		);
-		await page.close();
-
-		return htmlArray;
+		return arrayOfHtmlPriceTableRows;
 	} catch (error) {
 		console.log(error);
 	}
@@ -52,31 +62,32 @@ const formatData = async dataArray => {
 };
 
 const pageScraper = async bookInfoArray => {
-	const browser = await puppeteer.launch();
-
 	const resolvedBookPrices = await Promise.all(
 		bookInfoArray.map(async (bookInfo, index) => {
-			const isbn = bookInfo.isbn ? bookInfo.isbn : bookInfo.isbn13;
+			try {
+				const isbn = bookInfo.isbn ? bookInfo.isbn : bookInfo.isbn13;
 
-			if (isbn) {
-				// get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
-				const aisn = isbn.substring(isbn.length - 10);
+				if (isbn) {
+					// get Amazon Standard Identification Number - the book’s ISBN in its older, 10-digit version
+					const aisn = isbn.substring(isbn.length - 10);
+					const scrapedDataArray = await getData(aisn);
+					const amazonPrices = await formatData(scrapedDataArray);
 
-				const scrapedDataArray = await getData(aisn, browser);
-				const amazonPrices = await formatData(scrapedDataArray);
+					console.info(
+						`scraped and formatted prices for ${
+							bookInfo.title
+						}: ${index} of ${bookInfoArray.length}`
+					);
 
-				console.info(
-					`scraped and formatted prices for ${
-						bookInfo.title
-					}: ${index} of ${bookInfoArray.length}`
-				);
-
-				return {
-					title: bookInfo.title,
-					prices: amazonPrices
-				};
-			} else {
-				console.info(`no isbn for ${bookInfo.title}`);
+					return {
+						title: bookInfo.title,
+						prices: amazonPrices
+					};
+				} else {
+					console.info(`no isbn for ${bookInfo.title}`);
+				}
+			} catch (error) {
+				console.log(error);
 			}
 		})
 	);
