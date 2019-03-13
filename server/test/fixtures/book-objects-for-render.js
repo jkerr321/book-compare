e8130c15549cb056c73ae817b959c0f958ca8e22
Jennifer Kerr <jkerr321@gmail.com>
2019-03-13 15:50:14 +0000

    Remove Express and front end To make this into a purely csv export module

---
+++ server/test/fixtures/book-objects-for-render.js
@@ -0,0 +1,666 @@
+module.exports = [
+	{
+		title: 'Ottolenghi SIMPLE',
+		author: 'Yotam Ottolenghi',
+		average_rating: '4.24',
+		isbn: '1785031163',
+		isbn13: '9781785031168',
+		prices: {
+			kindle: { amazon: '9.99', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '15.47',
+				new_from: '13.66',
+				used_from: '13.64'
+			},
+			paperback: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Our Man in Havana',
+		author: 'Graham Greene',
+		average_rating: '3.94',
+		isbn: '0140184937',
+		isbn13: '9780140184938',
+		prices: {
+			kindle: { amazon: '0.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '6.38', new_from: '6.38', used_from: '5.00' },
+			paperback: { amazon: '9.99', new_from: '1.99', used_from: '0.65' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '—', new_from: '117.04', used_from: '117.05' }
+		}
+	},
+	{
+		title: 'The Heart of the Matter',
+		author: 'Graham Greene',
+		average_rating: '3.99',
+		isbn: '0099478420',
+		isbn13: '9780099478423',
+		prices: {
+			kindle: { amazon: '4.99', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '19.95',
+				new_from: '19.95',
+				used_from: '27.47'
+			},
+			paperback: { amazon: '8.31', new_from: '3.70', used_from: '1.34' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			mp: { amazon: '3', new_from: '3', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Quiet American',
+		author: 'Graham Greene',
+		average_rating: '3.97',
+		isbn: '0143039024',
+		isbn13: '9780143039020',
+		prices: {
+			kindle: { amazon: '4.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '40.95', used_from: '0.26' },
+			paperback: { amazon: '5.58', new_from: '1.45', used_from: '0.03' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '17.98', new_from: '17.98', used_from: '28.60' }
+		}
+	},
+	{
+		title: 'Graham Greene: A Life in Letters',
+		author: 'Graham Greene',
+		average_rating: '4.07',
+		isbn: '0316727938',
+		isbn13: '9780316727938',
+		prices: {
+			hardcover: { amazon: '—', new_from: '14.99', used_from: '0.01' },
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'A Sort Of Life',
+		author: 'Graham Greene',
+		average_rating: '3.70',
+		isbn: '0671782185',
+		isbn13: '9780671782184',
+		prices: {
+			kindle: { amazon: '5.49', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '0.01' },
+			paperback: { amazon: '8.99', new_from: '4.98', used_from: '2.10' }
+		}
+	},
+	{
+		title: 'Middlesex',
+		author: 'Jeffrey Eugenides',
+		average_rating: '3.99',
+		isbn: '0312422156',
+		isbn13: '9780312422158',
+		prices: {
+			kindle: { amazon: '4.29', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '8.85', new_from: '8.72', used_from: '6.29' },
+			paperback: { amazon: '7.77', new_from: '5.62', used_from: '1.00' },
+			audio: { amazon: '45.91', new_from: '27.45', used_from: '3.75' }
+		}
+	},
+	{
+		title: 'The Guernsey Literary and Potato Peel Pie Society',
+		author: 'Mary Ann Shaffer',
+		average_rating: '4.15',
+		isbn: '1984801813',
+		isbn13: '9781984801814',
+		prices: {
+			kindle: { amazon: '4.27', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '14.71', used_from: '0.01' },
+			paperback: { amazon: '5.01', new_from: '2.20', used_from: '0.01' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '32.09', new_from: '22.66', used_from: '22.65' },
+			cd: { amazon: '—', new_from: '1', used_from: '999.00' }
+		}
+	},
+	{
+		title: 'The Sisterhood: A Love Letter to the Women Who Have Shaped Me',
+		author: 'Daisy Buchanan',
+		average_rating: '4.60',
+		isbn: '1472238850',
+		isbn13: '9781472238856',
+		prices: {
+			kindle: { amazon: '7.99', new_from: '—', used_from: '—' },
+			paperback: { amazon: '10.42', new_from: '8.96', used_from: '9.53' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Life and Times of the Thunderbolt Kid',
+		author: 'Bill Bryson',
+		average_rating: '3.94',
+		isbn: '076791936X',
+		isbn13: '9780767919364',
+		prices: {
+			kindle: { amazon: '5.49', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '13.33', used_from: '0.01' },
+			paperback: { amazon: '6.48', new_from: '3.64', used_from: '0.01' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '14.99', new_from: '10.29', used_from: '3.72' }
+		}
+	},
+	{
+		title: 'The Argonauts',
+		author: 'Maggie Nelson',
+		average_rating: '4.03',
+		isbn: '1555977073',
+		isbn13: '9781555977078',
+		prices: {
+			hardcover: { amazon: '—', new_from: '—', used_from: '26.70' },
+			mass: { amazon: '—', new_from: '16.76', used_from: '—' },
+			audio: { amazon: '22.93', new_from: '22.93', used_from: '10.98' },
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Natives: Race and Class in the Ruins of Empire',
+		author: 'Akala',
+		average_rating: '4.61',
+		isbn: null,
+		isbn13: '9781473661219',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Color Purple',
+		author: 'Alice Walker',
+		average_rating: '4.19',
+		isbn: '0671727796',
+		isbn13: '9780671727796',
+		prices: {
+			kindle: { amazon: '5.99', new_from: '—', used_from: '—' },
+			library: { amazon: '20.61', new_from: '20.61', used_from: '46.19' },
+			paperback: { amazon: '5.99', new_from: '5.43', used_from: '5.08' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			mp: { amazon: '3', new_from: '3', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: "Frenchman's Creek",
+		author: 'Daphne du Maurier',
+		average_rating: '3.93',
+		isbn: '1844080412',
+		isbn13: '9781844080410',
+		prices: {
+			kindle: { amazon: '5.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '14.99', new_from: '8.99', used_from: '6.16' },
+			paperback: { amazon: '7.16', new_from: '5.27', used_from: '0.01' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '—', new_from: '27.28', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Diary of a Bookseller',
+		author: 'Shaun Bythell',
+		average_rating: '3.86',
+		isbn: '1781258635',
+		isbn13: '9781781258637',
+		prices: {
+			kindle: { amazon: '5.43', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '33.20', used_from: '3.59' },
+			paperback: { amazon: '6.01', new_from: '4.51', used_from: '4.65' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '14.87', new_from: '12.72', used_from: '13.84' }
+		}
+	},
+	{
+		title:
+			'Four Feet Under: Thirty Untold Stories of Homelessness in London',
+		author: 'Tamsen Courtenay',
+		average_rating: '4.42',
+		isbn: '178352572X',
+		isbn13: '9781783525720',
+		prices: {
+			kindle: { amazon: '3.20', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '12.85',
+				new_from: '10.06',
+				used_from: '12.45'
+			},
+			paperback: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title:
+			'Liquid Rules: The Delightful and Dangerous Substances That Flow Through Our Lives',
+		author: 'Mark Miodownik',
+		average_rating: '4.29',
+		isbn: '054485019X',
+		isbn13: '9780544850194',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title:
+			'Ten Arguments For Deleting Your Social Media Accounts Right Now',
+		author: 'Jaron Lanier',
+		average_rating: '3.58',
+		isbn: '1847925391',
+		isbn13: '9781847925398',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Widows of Malabar Hill (Perveen Mistry, #1)',
+		author: 'Sujata Massey',
+		average_rating: '3.93',
+		isbn: '1616957786',
+		isbn13: '9781616957780',
+		prices: {
+			kindle: { amazon: '6.48', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '13.88',
+				new_from: '11.86',
+				used_from: '13.82'
+			},
+			paperback: { amazon: '8.46', new_from: '6.05', used_from: '5.80' }
+		}
+	},
+	{
+		title: 'Make It Ahead: A Barefoot Contessa Cookbook',
+		author: 'Ina Garten',
+		average_rating: '4.12',
+		isbn: '0307464881',
+		isbn13: '9780307464880',
+		prices: {
+			kindle: { amazon: '16.99', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '18.99',
+				new_from: '16.30',
+				used_from: '14.00'
+			},
+			paperback: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Order of Time',
+		author: 'Carlo Rovelli',
+		average_rating: '4.08',
+		isbn: '073521610X',
+		isbn13: '9780735216105',
+		prices: {
+			kindle: { amazon: '8.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '8.17', new_from: '5.12', used_from: '5.00' },
+			paperback: { amazon: '7.91', new_from: '7.91', used_from: '—' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '16.22', new_from: '9.19', used_from: '10.71' }
+		}
+	},
+	{
+		title:
+			"She Has Her Mother's Laugh: The Powers, Perversions, and Potential of Heredity",
+		author: 'Carl Zimmer',
+		average_rating: '4.16',
+		isbn: '1101984597',
+		isbn13: '9781101984598',
+		prices: {
+			kindle: { amazon: '12.34', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '16.03',
+				new_from: '15.02',
+				used_from: '13.39'
+			},
+			paperback: { amazon: '11.43', new_from: '11.43', used_from: '—' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			}
+		}
+	},
+	{
+		title:
+			'Broad Band: The Untold Story of the Women Who Made the Internet',
+		author: 'Claire L.  Evans',
+		average_rating: '4.01',
+		isbn: '0735211752',
+		isbn13: '9780735211759',
+		prices: {
+			kindle: { amazon: '11.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '17.96', new_from: '5.52', used_from: '5.51' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			paperback: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Girl Who Smiled Beads: A Story of War and What Comes After',
+		author: 'Clemantine Wamariya',
+		average_rating: '4.21',
+		isbn: '0451495322',
+		isbn13: '9780451495327',
+		prices: {
+			kindle: { amazon: '4.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '11.20', new_from: '7.45', used_from: '2.25' },
+			paperback: { amazon: '9.99', new_from: '2.38', used_from: '5.47' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '9.66', new_from: '9.66', used_from: '—' }
+		}
+	},
+	{
+		title: 'Heart Berries',
+		author: 'Terese Marie Mailhot',
+		average_rating: '3.89',
+		isbn: '1619023342',
+		isbn13: '9781619023345',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Skin in the Game: Hidden Asymmetries in Daily Life',
+		author: 'Nassim Nicholas Taleb',
+		average_rating: '3.85',
+		isbn: '042528462X',
+		isbn13: '9780425284629',
+		prices: {
+			kindle: { amazon: '9.99', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '13.00',
+				new_from: '8.89',
+				used_from: '10.83'
+			},
+			paperback: { amazon: '6.99', new_from: '5.24', used_from: '3.99' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			}
+		}
+	},
+	{
+		title: 'Morvern Callar',
+		author: 'Alan Warner',
+		average_rating: '3.69',
+		isbn: '038548741X',
+		isbn13: '9780385487412',
+		prices: {
+			paperback: { amazon: '8.99', new_from: '5.26', used_from: '4.45' },
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Calypso',
+		author: 'David Sedaris',
+		average_rating: '4.21',
+		isbn: '0316392383',
+		isbn13: '9780316392389',
+		prices: {
+			kindle: { amazon: '6.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '13.21', new_from: '9.66', used_from: '7.25' },
+			paperback: { amazon: '8.79', new_from: '8.79', used_from: '—' },
+			audible: {
+				amazon: '0.00',
+				new_from: undefined,
+				used_from: undefined
+			},
+			audio: { amazon: '3', new_from: '21.25', used_from: '19.94' }
+		}
+	},
+	{
+		title: 'Fruit of the Drunken Tree',
+		author: 'Ingrid Rojas Contreras',
+		average_rating: '3.95',
+		isbn: '0385542720',
+		isbn13: '9780385542722',
+		prices: {
+			hardcover: {
+				amazon: '15.39',
+				new_from: '12.79',
+				used_from: '11.67'
+			},
+			paperback: { amazon: '12.29', new_from: '12.29', used_from: '—' },
+			kindle: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Transcription',
+		author: 'Kate Atkinson',
+		average_rating: '3.56',
+		isbn: '031617663X',
+		isbn13: '9780316176637',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title:
+			'The Food Explorer: The True Adventures of the Globe-Trotting Botanist Who Transformed What America Eats',
+		author: 'Daniel Stone',
+		average_rating: '4.07',
+		isbn: '1101990589',
+		isbn13: '9781101990582',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Aran Islands',
+		author: 'J.M. Synge',
+		average_rating: '3.81',
+		isbn: '0140184325',
+		isbn13: '9780140184327',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Library Book',
+		author: 'Susan Orlean',
+		average_rating: '4.06',
+		isbn: '1476740186',
+		isbn13: '9781476740188',
+		prices: {
+			kindle: { amazon: '7.19', new_from: '—', used_from: '—' },
+			hardcover: {
+				amazon: '13.54',
+				new_from: '9.09',
+				used_from: '11.94'
+			},
+			paperback: { amazon: '—', new_from: '9.81', used_from: '—' },
+			audio: { amazon: '37.08', new_from: '25.92', used_from: '28.43' }
+		}
+	},
+	{
+		title: 'The People in the Trees',
+		author: 'Hanya Yanagihara',
+		average_rating: '3.68',
+		isbn: '0385536771',
+		isbn13: '9780385536776',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		author: 'Andy Hunt',
+		average_rating: '4.31',
+		isbn: '020161622X',
+		isbn13: '9780201616224',
+		prices: {
+			kindle: { amazon: '14.85', new_from: '—', used_from: '—' },
+			paperback: {
+				amazon: '36.99',
+				new_from: '27.30',
+				used_from: '27.99'
+			},
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'The Most of Nora Ephron',
+		author: 'Nora Ephron',
+		average_rating: '4.26',
+		isbn: '038535083X',
+		isbn13: '9780385350839',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'I Feel Bad about My Neck: And Other Thoughts on Being a Woman',
+		author: 'Nora Ephron',
+		average_rating: '3.68',
+		isbn: '0307264556',
+		isbn13: '9780307264558',
+		prices: {
+			kindle: { amazon: '4.99', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '28.11', used_from: '0.01' },
+			paperback: { amazon: '6.47', new_from: '3.56', used_from: '0.95' },
+			audio: { amazon: '13.62', new_from: '10.24', used_from: '59.00' }
+		}
+	},
+	{
+		title: "Things I Don't Want to Know",
+		author: 'Deborah Levy',
+		average_rating: '4.19',
+		isbn: '1907903631',
+		isbn13: '9781907903632',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Little Fires Everywhere',
+		author: 'Celeste Ng',
+		average_rating: '4.13',
+		isbn: '0735224293',
+		isbn13: '9780735224292',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Books v. Cigarettes',
+		author: 'George Orwell',
+		average_rating: '3.92',
+		isbn: '0141036613',
+		isbn13: '9780141036618',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Why I Write',
+		author: 'George Orwell',
+		average_rating: '4.03',
+		isbn: '0143036351',
+		isbn13: '9780143036357',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Coming Up for Air',
+		author: 'George Orwell',
+		average_rating: '3.79',
+		isbn: '0156196255',
+		isbn13: '9780156196253',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title:
+			'Facts Are Subversive: Political Writing from a Decade without a Name',
+		author: 'Timothy Garton Ash',
+		average_rating: '3.72',
+		isbn: '1848870892',
+		isbn13: '9781848870895',
+		prices: {
+			kindle: { amazon: '—', new_from: '—', used_from: '—' },
+			paperback: { amazon: '—', new_from: '—', used_from: '—' },
+			hardcover: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	},
+	{
+		title: 'Dancing Fish and Ammonites: A Memoir',
+		author: 'Penelope Lively',
+		average_rating: '3.61',
+		isbn: '0670016551',
+		isbn13: '9780670016556',
+		prices: {
+			hardcover: { amazon: '—', new_from: '5.00', used_from: '0.01' },
+			paperback: { amazon: '9.31', new_from: '5.04', used_from: '0.01' },
+			audio: { amazon: '—', new_from: '78.99', used_from: '67.00' },
+			kindle: { amazon: '—', new_from: '—', used_from: '—' }
+		}
+	}
+];
