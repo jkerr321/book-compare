04cf89ff623d9cb95279c04f4049a04b3dce3040
Jennifer Kerr <jkerr321@gmail.com>
2019-04-17 18:39:23 +0100

    fix conflicts

--- server/index.js
+++ server/index.js
@@ -24,11 +24,7 @@ const isScrapeWithinPreviousDay = (timestampNow, csvName) => {
 	}
 }
 
-<<<<<<< HEAD
 const init = async () => {
-=======
-const init = async (config) => {
->>>>>>> master
 	const timestampNow = new Date().valueOf();
 	const existingCsv = getCsvName();
 
@@ -38,12 +34,8 @@ const init = async (config) => {
 		if (pricesScrapedInLast24Hours) {
 			console.log('prices scraped in the last 24 hours - please use existing output.csv file');
 		} else {
-<<<<<<< HEAD
-			// const toReadList = await getGoodReadBooks(); // production values
-=======
-			// const toReadList = await getGoodReadBooks(config); // production values
->>>>>>> master
-			const toReadList = goodReadsTestResponse; // test values
+			const toReadList = await getGoodReadBooks(); // production values
+			// const toReadList = goodReadsTestResponse; // test values
 			const bookPricesArray = await scrapePrices(toReadList);
 
 			const bookDetailsWithPrices = bookPricesArray.map(amazonPriceObject => {
